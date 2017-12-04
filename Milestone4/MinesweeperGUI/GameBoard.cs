266127f2dfbeb331b8ee287a0c1a416784ce6517
m-c-rogers <mcrogers08@gmail.com>
2017-12-03 18:23:44 -0700

    Add files via upload

---
+++ Milestone4/MinesweeperGUI/GameBoard.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Created by Michael Rogers - December 2017 - CST 227 taught by Professor Lori Collier
+
+namespace MinesweeperGUI
+{
+    public partial class GameBoard : Form
+    {
+
+        public GameBoard()
+        {
+            InitializeComponent();
+        }
+
+        public GameBoard(int x, int y)
+        {
+            InitializeComponent();
+            //Generate board size based on size passed in from StartScreen.
+            if (x == 10)
+            {
+                this.Width = 225;
+                this.Height = 248;
+            }
+            else if (x == 15)
+            {
+                this.Width = 330;
+                this.Height = 353;
+            }
+            else if ( x == 20)
+            {
+                this.Width = 435;
+                this.Height = 458;
+            }
+            //Prevent resizing of form
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+
+            //New array of buttons
+            clickableButton[,] buttons = new clickableButton[x, y];
+
+            //Populate form with buttons.
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    buttons[i, j] = new clickableButton(21 * i, 21 * j);
+                    this.Controls.Add(buttons[i, j]);
+                }
+            }
+        }
+
+        private void GameBoard_Deactivate(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
+
---
+++ Milestone4/MinesweeperGUI/clickableButton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Created by Michael Rogers - December 2017 - CST 227 taught by Professor Lori Collier
+
+namespace MinesweeperGUI
+{
+    class clickableButton : Button
+    {
+        const int height = 20;
+        const int width = 20;
+
+        //Constructors
+        public clickableButton()
+        {
+
+        }
+
+        public clickableButton(int xCoord, int yCoord)
+        {
+            this.Location = new Point(xCoord, yCoord);
+            this.Height = height;
+            this.Width = width;
+            this.FlatStyle = FlatStyle.Popup;
+        }
+
+        //Add a blue border and flatten button when clicked.
+        protected override void OnClick(EventArgs e)
+        {
+            base.OnClick(e);
+            this.FlatStyle = FlatStyle.Flat;
+            this.FlatAppearance.BorderSize = 2;
+            this.FlatAppearance.BorderColor = Color.Blue;
+        }
+    }
+}
